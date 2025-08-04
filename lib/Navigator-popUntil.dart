84bb7a50ff1804551d3e6ead6b9f0df7c6232618
ChornChhery <chherychorn@gmail.com>
2025-08-04 14:11:24 +0700

    Pop until

---
+++ lib/Navigator-popUntil.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/page1': (context) => Page1(),
+        '/page2': (context) => Page2(),
+        '/page3': (context) => Page3(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(' Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Page 1'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/page1');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Page 1')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Page 2'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/page2');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Page 2')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Page 3'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/page3');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Page 3')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Back to Home Page'),
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+        ),
+      ),
+    );
+  }
+}
